7e27c2df341e0bfd0357c9971b6d578eaa1c7819
Shahwar Spall <shevy.spall@gmail.com>
2019-11-13 02:54:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,13 @@ def reverse_each_word(string)
 end
 array.join(" ")
 end
+
+
+def reverse_each_word(string)
+ string= string.split
+ array=[]
+ string.collect do |back|
+   array << back.reverse 
+end
+array.join(" ")
+end
